ab88fc406c1fc06d57689c9d04dfd7c8a2c56db5
IgorAlves <igor.alves@whitespace.ch>
2023-04-13 10:26:12 +0100

    initial config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans';
+import theme from './src/themes';
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </ThemeProvider>
   );
 }
 
